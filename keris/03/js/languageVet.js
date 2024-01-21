e7e526653711c1c1e24ea9d39fcfacdcf62a2ec8
jjh1129jh <82186984+jjh1129jh@users.noreply.github.com>
2024-01-21 23:03:31 +0900

    Add files via upload

---
+++ keris/03/js/languageVet.js
@@ -0,0 +1,38 @@
+//베트남어
+var Vietnamese1=[
+	"Chọn O nếu nội dung đúng với đoạn hội thoại trước đó, sai thì chọn X."
+]
+var Vietnamese2=[
+	"Chọn O nếu nội dung đúng với đoạn hội thoại trước đó, sai thì chọn X."
+]
+var Vietnamese3=[
+	"Chọn O nếu nội dung đúng với đoạn hội thoại trước đó, sai thì chọn X."
+]
+var Vietnamese4=[
+	"Chọn O nếu nội dung đúng với đoạn hội thoại trước đó, sai thì chọn X."
+]
+var Vietnamese5=[
+	"Chọn O nếu nội dung đúng với đoạn hội thoại trước đó, sai thì chọn X."
+]
+var Vietnamese6=[
+	"Chọn O nếu nội dung đúng với đoạn hội thoại trước đó, sai thì chọn X."
+]
+var Vietnamese7=[
+	"Chọn O nếu nội dung đúng với đoạn hội thoại trước đó, sai thì chọn X."
+]
+var Vietnamese8=[
+	"Chọn O nếu nội dung đúng với đoạn hội thoại trước đó, sai thì chọn X.",
+]
+var Vietnamese9=[
+	"Chọn O nếu nội dung đúng với đoạn hội thoại trước đó, sai thì chọn X."
+]
+var Vietnamese10=[
+	"Chọn O nếu nội dung đúng với đoạn hội thoại trước đó, sai thì chọn X."
+]
+var Vietnamese11=[
+	"Chọn O nếu nội dung đúng với đoạn hội thoại trước đó, sai thì chọn X."
+]
+var Vietnamese12=[
+	"Chọn O nếu nội dung đúng với đoạn hội thoại trước đó, sai thì chọn X."
+]
+//베트남어
---
+++ keris/03/js/quizInfo.js
@@ -0,0 +1,109 @@
+//********** 총 페이지 수 **********
+var videoWarpData = [
+    "1. ",
+    "2. ",
+    "3. ",
+]
+
+var videoWarpTime = [
+    0,
+	372,
+	1267,
+]
+
+// 학습목표 내용
+var topText = [
+	"1. ",
+    "2. ",
+    "3. ",
+]
+var bottomText = [
+	"1. ",
+	"2. ",
+	"3. ",
+]
+
+
+
+// 정리하기 내용
+var orgData = [
+	[
+		{
+			title: '1.',
+			content: [
+			]
+		},
+	],
+]
+
+
+var organizeImageCount = orgData.length;
+
+//********** 대메뉴에 속한 소메뉴 개수 **********
+var bigMenuArr = [2, 2, 2];
+
+
+//A - 객관식
+//B - 주관식
+//C - OX
+var quizType = ["B", "B", "B"];
+var answer = ['o', 'x', 'x'];
+
+var quizTitleTxt = [
+	"다음 문장이 앞의 대화와 같으면 O, 다르면 X를 고르세요.",
+	"다음 문장이 앞의 대화와 같으면 O, 다르면 X를 고르세요.",
+	"다음 문장이 앞의 대화와 같으면 O, 다르면 X를 고르세요.",
+	"",
+]
+
+var addquiztitle = [
+	"나은이는 뉴스를 봤습니다.",
+	"세계 곳곳에서 비가 안 오고 있습니다.",
+	"바얀의 고향에 비가 너무 많이 왔습니다.",
+	""
+]
+
+var selectList = [
+	[
+		"선택",
+		"선택",
+		"선택",
+		"선택",
+	],
+	[
+		"선택",
+		"선택",
+		"선택",
+		"선택",
+	],
+	[
+		"선택",
+		"선택",
+		"선택",
+		"선택",
+	],
+];
+
+//********** 퀴즈 해설 **********
+var explainArr = [
+	"나은이는 뉴스를 봤습니다.",
+	"세계 곳곳에서 비가 안 오고 있습니다.",
+	"바얀의 고향에 비가 너무 많이 왔습니다.",
+	"",
+];
+
+
+//********** 각 페이지별 북마크 시간 **********			
+var bookmarkTImeArr = new Array();
+bookmarkTImeArr[2] = [0, 45, 90];
+
+
+//********** 각 차시, 페이지별 북마크 이름 **********
+var bookmarkNameArr = new Array();
+bookmarkNameArr[2] = [""];
+
+
+
+document.addEventListener('load', function(){
+
+})
