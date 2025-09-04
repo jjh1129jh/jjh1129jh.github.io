b8a7be953fa2aefad699e59ae84cb5c1e720123d
jjh1129jh <82186984+jjh1129jh@users.noreply.github.com>
2025-09-04 19:45:13 +0900

    Add files via upload

---
+++ keris/03/js/languageEng.js
@@ -0,0 +1,43 @@
+//영어
+var English1 =[
+    "Choose O if the following sentence is the same as the previous conversation, or X if it is different."
+]
+var English2 =[
+    "Choose O if the following sentence is the same as the previous conversation, or X if it is different.",
+]
+var English3 =[
+    "Choose O if the following sentence is the same as the previous conversation, or X if it is different.",
+]
+var English4 =[
+    "Choose O if the following sentence is the same as the previous conversation, or X if it is different."
+]
+var English5 =[
+    "Choose O if the following sentence is the same as the previous conversation, or X if it is different."
+]
+var English6 =[
+    "Choose O if the following sentence is the same as the previous conversation, or X if it is different.",
+     
+]
+var English7 =[
+    "Choose O if the following sentence is the same as the previous conversation, or X if it is different."
+]
+var English8 =[
+    "Choose O if the following sentence is the same as the previous conversation, or X if it is different."
+]
+var English9 =[
+    "Choose O if the following sentence is the same as the previous conversation, or X if it is different.",
+    "Choose O if the following sentence is the same as the previous conversation, or X if it is different.",
+    "Choose O if the following sentence is the same as the previous conversation, or X if it is different.",
+]
+var English10 =[
+    "Choose O if the following sentence is the same as the previous conversation, or X if it is different."
+]
+var English11 =[
+    "Choose O if the following sentence is the same as the previous conversation, or X if it is different."
+]
+var English12 =[
+    "Choose O if the following sentence is the same as the previous conversation, or X if it is different."
+]
+
+//영어
+//언어별 텍스트
